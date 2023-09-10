c254238a91eb7368bf603b10f2292db61d037716
ashley-eze <ashleynonye4@gmail.com>
2023-09-09 22:21:14 -0500

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Prints all possible combinations of single-digit numbers. Numbers are separated by ', ' and are printed in ascending order.
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int dig = 0;
+while (dig <= 9)
+{
+putchar(dig + '0'); 
+if (dig < 9)
+{
+putchar(',');
+putchar(' ');
+}
+dig++;
+}
+
+putchar('\n');
+
+return (0);
+}
