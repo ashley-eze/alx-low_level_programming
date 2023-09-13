f7251ffd10d2bcb22ea49a90e02321d6ca879217
ashley-eze <ashleynonye4@gmail.com>
2023-09-13 02:36:26 +0100

    lower

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n;
+
+ n = _islower('H');
+_putchar(n + '0');
+n = _islower('o');
+_putchar(n + '0');
+n = _islower(108);
+_putchar(n + '0');
+_putchar('\n');
+return (0);
+}
