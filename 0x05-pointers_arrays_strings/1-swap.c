e5f83e3a8b7132054a86755ff8c5a567e688738f
ashley-eze <ashleynonye4@gmail.com>
2023-09-18 23:47:11 +0100

    bat

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
-*swap_int(int *a, int *b): swaps
-*@a-alphabet to be swapped with b
-*@b-tobe swapped with a
+*swap_int(int *a, int *b)- swaps
+*@a: pointer for integer one
+*@b: pointer for integer two
+*Description: This function swaps the values of two integers
 */
 void swap_int(int *a, int *b)
 {
