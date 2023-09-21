8aabd4f6cdc6db648aed30937cc4f58c9f718c95
ashley-eze <ashleynonye4@gmail.com>
2023-09-22 00:59:29 +0100

    seven

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,19 +10,19 @@
 
 char *leet(char *n)
 {
-	int x, w;
-	char s1[] = "aAeEoOtTlL";
-	char s2[] = "4433007711";
+int x, w;
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
 
-	for (x = 0; n[x] != '\0'; x++)
-	{
-	for (w = 0; w < 10; w++)
-	{
-	if (n[x] == s1[w])
-	{
-	n[x] = s2[w];
-	}
-	}
-	}
-	return ("n");
+for (x = 0; n[x] != '\0'; x++)
+{
+for (w = 0; w < 10; w++)
+{
+if (n[x] == s1[w])
+{
+n[x] = s2[w];
+}
+}
+}
+return (n);
 }
